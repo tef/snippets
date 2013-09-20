6e13186554268cd373a76a6160ea5e478afa4336
tef <tef@twentygototen.org>
2013-09-20 16:48:28 +0100

    Add config

---
+++ config.py
@@ -0,0 +1,15 @@
+reminder_body = """
+Hello,
+
+1. What happened this week? (What did you work on? What did you achieve? Did you meet any interesting people? etc)
+2. What are your plans and priorities for next week? (Events, major meetings, deadlines)
+3. Is there anything you'd like help with from the other team members? 
+
+"""
+reminder_subject="What happened this week?"
+
+digest_subject="What happened last week at Code Club"
+
+domain = "codeclubsnippets.appspotmail.com"
+email = "codebot@%s" % domain
+email_address="CodeBot <%s>"%email
